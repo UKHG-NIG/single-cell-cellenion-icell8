b15c43ad77cd94c31506ad9d84b6371e2779dbe8
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-02 15:28:55 +0100

    Update generate_CogentDS_reports.R

--- figure2/generate_CogentDS_reports.R
+++ figure2/generate_CogentDS_reports.R
@@ -1,8 +1,21 @@
 require(CogentDS)
 
 # ICELL8 dataset
+## Create text file referring to gene matrix files
+fileConn<-file("GSM5411464_geneMatrix_files.txt")
+writeLines(c("GSM5411464_ICELL8_geneMatrix_chip1.csv","GSM5411465_ICELL8_geneMatrix_chip2.csv","GSM5411466_ICELL8_geneMatrix_chip3.csv"), fileConn)
+close(fileConn)
+## Create text file referring to metadata files
+fileConn<-file("GSM5411464_metadata_files.txt")
+writeLines(c("GSM5411464_ICELL8_metadata_chip1.csv","GSM5411465_ICELL8_metadata_chip2.csv","GSM5411466_ICELL8_metadata_chip3.csv"), fileConn)
+close(fileConn)
 
-# Default analysis for samples
-standard_analysis(gm = "analysis_L4_old/analysis_L4_genematrix.csv", metadata = "analysis_L4_old/analysis_L4_new_stats.csv",
-                  gene_info_loc=geneInfo, cluster_analysis=T, grouping_var="Sample",
-                  parallel_cores=100, report=T, CogentAP_data = T, output_dir = file.path(OutDir,"CogentDS"))
+standard_analysis(gm_loc = "GSM5411464_geneMatrix_files.txt", metadata_loc = "GSM5411464_metadata_files.txt", gene_info_loc="GSE179204_gene_info.csv",
+                  cluster_analysis=T, grouping_var="Sample", names=c("chip1","chip2","chip3"), report=T, CogentAP_data = T,
+                  output_dir = "CogentDS_ICELL8", mito_frac=c(0,1), intergenic_frac=c(0,1))
+
+# CellenONE/ICELL8 dataset
+## Default analysis for samples
+standard_analysis(gm = "GSM5411467_CellenONE_geneMatrix.csv", metadata = "GSM5411467_CellenONE_metadata.csv", gene_info_loc="GSM5411467_gene_info.csv",
+                  cluster_analysis=T, grouping_var="Sample", report=T, CogentAP_data = T, output_dir = "CogentDS_CellenONE",
+                  mito_frac=c(0,1), intergenic_frac=c(0,1))
