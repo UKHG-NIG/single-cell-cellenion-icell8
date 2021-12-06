e43b5d45dc9a790758a0b6ff587e03557f713e6a
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-06 15:48:41 +0100

    Update figure2d.R

--- figure2/figure2d.R
+++ figure2/figure2d.R
@@ -1,1 +1,46 @@
+# Generate Venn diagram and violin plots comparing number of genes detected in each approach
+setwd("/path/to/InputDir")
+
+# Load supporting libraries
+require(VennDiagram)
+require(reshape2)
+
+# Load datasets
+## Composite approach
+composite <- get(load("CogentDS_CellenONE/CogentDS.analysis.rda"))
+## ICELL8 approach
+icell8 <- get(load("CogentDS_ICELL8/CogentDS.analysis.rda"))
+
+# Plot Venn diagram
+grid.draw(venn.diagram(x = list("CellenONE-ICELL8"=rownames(composite$qc_data$gm),
+                                "ICELL8"=rownames(icell8$qc_data$gm)), col = "transparent",
+                       fill = c("blue", "yellow"),# fill = c("blue", "green","red"),
+                       alpha = 0.5, cat.cex=1.5, cex=1.5, height=100, width=100,filename=NULL,
+                       print.mode=c("raw","percent")))
+
+# Plot violin plot for genes detected per cell
+# Raw reads counted for different platforms 
+## Icell8 
+icell8_mat <- icell8$qc_data$metadata[,c("Sample","Barcoded_Reads","Exon_Reads","No_of_Genes")]
+icell8_norm <- icell8$qc_data$gm
+icell8_mat$No_of_Genes_CPM01 <- apply(icell8_norm, 2, function(x) sum(x>0.1))
+icell8_mat$No_of_Genes_CPM1 <- apply(icell8_norm, 2, function(x) sum(x>1))
+icell8_mat$platform="ICELL8"
+icell8_mat <- icell8_mat[!(icell8_mat$Sample %in% c("Pos_Ctrl","Neg_Ctrl")),]
+icell8_mat$Sample <- tolower(icell8_mat$Sample)
+## Cellenion 
+cellenion_mat <- composite$qc_data$metadata[,c("Sample","Barcoded_Reads","Exon_Reads","No_of_Genes")]
+cellenion_norm <- composite$qc_data$gm
+cellenion_mat$No_of_Genes_CPM01 <- apply(cellenion_norm, 2, function(x) sum(x>0.1))
+cellenion_mat$No_of_Genes_CPM1 <- apply(cellenion_norm, 2, function(x) sum(x>1))
+cellenion_mat$platform="CellenONE"
+cellenion_mat <- cellenion_mat[!(cellenion_mat$Sample %in% "Goe247_wo__fix"),]
+cellenion_mat$Sample <- tolower(cellenion_mat$Sample)
+# Plot 
+plotDF <- melt(rbind(icell8_mat, cellenion_mat))
+ggplot(plotDF[plotDF$variable=="No_of_Genes",], aes(factor(platform), value, fill=platform))+
+  geom_violin(alpha=0.5) + scale_y_log10() +geom_boxplot(width = 0.2, fill="white")+theme_bw()+
+  scale_fill_manual(values = c("blue", "yellow"))+
+  theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+
+  labs(title="Number of genes expressed in cells (read count >0)", x="Platform", y="#Genes")
 
