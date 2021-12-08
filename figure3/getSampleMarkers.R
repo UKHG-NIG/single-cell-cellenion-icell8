2c3504c776a627fb5df01c20c867d0a39eb54ce3
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-08 14:58:56 +0100

    Update getSampleMarkers.R

--- figure3/getSampleMarkers.R
+++ figure3/getSampleMarkers.R
@@ -6,10 +6,10 @@ setwd("/path/to/InputDir")
 library(CogentDS)
 
 # Load RDA objects from CogentDS analyses
-Cogent_cellenion <- get(load("CogentDS_CellenONE/CogentDS.analysis.rda"))
-Cogent_icell8 <- get(load("CogentDS_ICELL8/CogentDS.analysis.rda"))
+composite <- get(load("CogentDS_CellenONE/CogentDS.analysis.rda"))
+icell8 <- get(load("CogentDS_ICELL8/CogentDS.analysis.rda"))
 
-# Get markers for icell8
+# Get markers for ICELL8 data
 icell8_samples <- grep(pattern="_Ctrl", Cogent_icell8$qc_data$metadata$Sample, invert=T)
 icell8_gm <- Cogent_icell8$qc_data$gm[,icell8_samples]
 icell8_metadata <- Cogent_icell8$qc_data$metadata[icell8_samples,]
@@ -24,20 +24,20 @@ icell8_markers <- lapply(icell8_markers, function(obj){
 })
 names(icell8_markers) <- toupper(names(icell8_markers))
 
-# Get markers for cellenion
-cellenion_samples <- grep(pattern="Goe247_wo__fix", Cogent_cellenion$qc_data$metadata$Sample, invert=T)
-cellenion_gm <- Cogent_cellenion$qc_data$gm[,cellenion_samples]
-cellenion_metadata <- Cogent_cellenion$qc_data$metadata[cellenion_samples,]
-cellenion_markers <- de.markers(gm = cellenion_gm,
-                             metadata = cellenion_metadata,
+# Get markers for composite approach data
+composite_samples <- grep(pattern="Goe247_wo__fix", composite$qc_data$metadata$Sample, invert=T)
+composite_gm <- composite$qc_data$gm[,composite_samples]
+composite_metadata <- composite$qc_data$metadata[composite_samples,]
+composite_markers <- de.markers(gm = composite_gm,
+                             metadata = composite_metadata,
                              min_fc = 0,
                              grouping_var = "Sample",
-                             transform_type = Cogent_cellenion$qc_data$params$gm_log_base)
-cellenion_markers <- lapply(cellenion_markers, function(obj){
+                             transform_type = composite$qc_data$params$gm_log_base)
+composite_markers <- lapply(composite_markers, function(obj){
   obj$rank <- -log10(obj$pval)*obj$log_fc
   obj[order(obj$rank, decreasing = T),]
 })
-names(cellenion_markers) <- toupper(names(cellenion_markers))
+names(composite_markers) <- toupper(names(composite_markers))
 
 # Print out markers for each sample
 for(sample in names(icell8_markers)){
@@ -46,8 +46,8 @@ for(sample in names(icell8_markers)){
               file = paste("markers_icell8_",sample,".csv", sep=""),
               sep=",", quote = F, row.names=T, col.names = NA)
   
-  # CellenIon markers
-  write.table(cellenion_markers[[sample]],
+  # Composite markers
+  write.table(composite_markers[[sample]],
               file = paste("markers_composite_",sample,".csv", sep=""),
               sep=",", quote = F, row.names=T, col.names = NA)
 }
