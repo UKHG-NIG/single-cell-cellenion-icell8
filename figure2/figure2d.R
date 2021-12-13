cb57617a8329dfb412ebff09dcaa4fc4f425df27
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-13 21:46:55 +0100

    Update figure2d.R

--- figure2/figure2d.R
+++ figure2/figure2d.R
@@ -14,6 +14,7 @@ icell8 <- get(load("CogentDS_ICELL8/CogentDS.analysis.rda"))
 # Plot Venn diagram for quality-control (QC) genes between the approaches
 grid.draw(venn.diagram(x = list("CellenONE-ICELL8"=rownames(composite$qc_data$gm),
                                 "ICELL8"=rownames(icell8$qc_data$gm)), col = "transparent",
+                       fontfamily ="sans",cat.fontfamily="sans",
                        fill = c("blue", "yellow"),# fill = c("blue", "green","red"),
                        alpha = 0.5, cat.cex=1.5, cex=1.5, height=100, width=100,filename=NULL,
                        print.mode=c("raw","percent")))
