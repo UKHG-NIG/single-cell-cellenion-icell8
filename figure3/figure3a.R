5af390440b7f83f2c1b6b041dc5e7cc22a5cdfb5
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-14 15:19:52 +0100

    Update figure3a.R

--- figure3/figure3a.R
+++ figure3/figure3a.R
@@ -5,22 +5,7 @@ library(ggpubr)
 samples <- c("GOE1303","GOE1305","GOE1309","GOE1360","GOE247","GOE486","GOE615","GOE800")
 for(sample in samples){
   # Load markers for each sample in each dataset
-  markersComposite <- read.table(paste("markers_composite_",sample,".csv",sep=""), sep = ",", header=T, row.names = 1)
-  markersICELL8 <- read.table(paste("markers_icell8_",sample,".csv",sep=""), sep = ",", header=T, row.names = 1)
-  colnames(markersICELL8) <- paste("icell8",colnames(markersICELL8),sep="_")
-  colnames(markersComposite) <- paste("cellenion",colnames(markersComposite),sep="_")
-  
-  # Merge marker tables together
-  df <- merge(markersICELL8, markersComposite, by="row.names",all=T)
-  df <- df[complete.cases(df), ]
-  # Sort markers by sum of absolute rank scores in both approaches
-  df <- df[order(abs(df$icell8_rank)+abs(df$cellenion_rank), decreasing=TRUE),]
-  # Select top 100 markers to plot
-  df <- df[1:100,]
-  write.table(df[,-1],sep=",",
-              file = paste("top100_markers_",sample,".csv", sep=""),
-              quote = F, row.names=df$Row.names, col.names = NA)
-  
+  df <- read.table(paste("top100_markers_",sample,".csv",sep=""),sep=",", header=T, row.names=1)
   plotDF <- data.frame(icell8=log(df[[grep("icell8",grep(sample,colnames(df), ignore.case=T, value=T), value=T)]]),
                        cellenion=log(df[[grep("cellenion",grep(sample,colnames(df), ignore.case=T, value=T), value=T)]]))
   
