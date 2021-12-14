9e90fe96683e651ec8707185a7386625340dd770
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-14 10:34:59 +0100

    Update getSampleMarkers.R

--- figure3/getSampleMarkers.R
+++ figure3/getSampleMarkers.R
@@ -10,14 +10,14 @@ composite <- get(load("CogentDS_CellenONE/CogentDS.analysis.rda"))
 icell8 <- get(load("CogentDS_ICELL8/CogentDS.analysis.rda"))
 
 # Get markers for ICELL8 data
-icell8_samples <- grep(pattern="_Ctrl", Cogent_icell8$qc_data$metadata$Sample, invert=T)
-icell8_gm <- Cogent_icell8$qc_data$gm[,icell8_samples]
-icell8_metadata <- Cogent_icell8$qc_data$metadata[icell8_samples,]
+icell8_samples <- grep(pattern="_Ctrl", icell8$qc_data$metadata$Sample, invert=T)
+icell8_gm <- icell8$qc_data$gm[,icell8_samples]
+icell8_metadata <- icell8$qc_data$metadata[icell8_samples,]
 icell8_markers <- de.markers(gm = icell8_gm,
                              metadata = icell8_metadata,
                              min_fc = 0,
                              grouping_var = "Sample",
-                             transform_type = Cogent_icell8$qc_data$params$gm_log_base)
+                             transform_type = icell8$qc_data$params$gm_log_base)
 icell8_markers <- lapply(icell8_markers, function(obj){
   obj$rank <- -log10(obj$pval)*obj$log_fc
   obj[order(obj$rank, decreasing = T),]
@@ -50,4 +50,19 @@ for(sample in names(icell8_markers)){
   write.table(composite_markers[[sample]],
               file = paste("markers_composite_",sample,".csv", sep=""),
               sep=",", quote = F, row.names=T, col.names = NA)
+  
+  # Merge marker tables together
+  markersICELL8 <- icell8_markers[[sample]]
+  markersComposite <- composite_markers[[sample]]
+  colnames(markersICELL8) <- paste("icell8",colnames(markersICELL8),sep="_")
+  colnames(markersComposite) <- paste("cellenion",colnames(markersComposite),sep="_")
+  df <- merge(markersICELL8, markersComposite, by="row.names",all=T)
+  df <- df[complete.cases(df), ]
+  # Sort markers by sum of absolute rank scores in both approaches
+  df <- df[order(abs(df$icell8_rank)+abs(df$cellenion_rank), decreasing=TRUE),]
+  # Select top 100 markers to plot
+  df <- df[1:100,]
+  write.table(df[,-1],sep=",",
+              file = paste("top100_markers_",sample,".csv", sep=""),
+              quote = F, row.names=df$Row.names, col.names = NA)
 }
