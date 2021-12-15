07286a93d4508420f883e187ed38fd7e62a68c93
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-15 15:12:34 +0100

    Update figure4cde.R

--- figure4/figure4cde.R
+++ figure4/figure4cde.R
@@ -1,5 +1,7 @@
 # This scripts is used to generate tSNE plots for composite approach and ICELL8 platform
 library(CogentDS)
+FDR=0.05
+FC=0.5
 
 composite <- get(load("CogentDS_CellenONE_fig4/CogentDS.analysis.rda"))
 icell8 <- get(load("CogentDS_ICELL8_fig4/CogentDS.analysis.rda"))
@@ -20,66 +22,111 @@ reduction.plot(vis_obj = composite[["vis_data"]], gm=composite[["qc_data"]]$gm,
                gene_highlight = "ENSG00000184752_NDUFA12", file="Fig4D_composite_NDUFA12_tSNE.pdf", marker_size=0.5, alpha=1)
 
 # Figure 4E: heatmap of top 30 markers based on their ranks ####
+# Obtain markers for each cluster in the composite approach
 composite_markers <- de.markers(gm = composite$qc_data$gm,
-                  metadata =  composite$qc$metadata,
-                  min_fc = 0,
-                  grouping_var = "Graph-based_Clusters",
-                  transform_type = composite$qc_data$params$gm_log_base)
-for(i in 1:length(res)){
-  colnames(composite_markers[[i]])[colnames(composite_markers[[i]])=="pval"] <- "pvalue"
-  colnames(composite_markers[[i]])[colnames(composite_markers[[i]])=="log_fc"] <- "log2FoldChange"
-  composite_markers[[i]]$rank <- -log10(composite_markers[[i]]$pvalue)*res[[i]]$log2FoldChange
-  tmpRes <- composite_markers[[i]][order(composite_markers[[i]]$rank), ]
+                                metadata =  composite$qc$metadata,
+                                min_fc = 0,
+                                grouping_var = "Graph-based_Clusters",
+                                transform_type = composite$qc_data$params$gm_log_base)
+# Create tables of markers tested for each cluster
+for(i in 1:length(composite_markers)){
+  sample <- names(composite_markers)[i]
+  obj <- composite_markers[[i]]
+  colnames(obj)[colnames(obj)=="pval"] <- "pvalue"
+  colnames(obj)[colnames(obj)=="log_fc"] <- "log2FoldChange"
+  obj$pvalue[obj$pvalue==0] <- .Machine$double.xmin
+  obj$rank <- -log10(obj$pvalue)*obj$log2FoldChange
+  tmpRes <- obj[order(obj$rank), ]
   gene_id <- sapply(rownames(tmpRes), function(gene) strsplit(x = gene, split="_")[[1]][1])
   Gene_Name <- sapply(rownames(tmpRes), function(gene) strsplit(x = gene, split="_")[[1]][2])
-  composite_markers[[i]] <- as.data.frame(cbind(gene_id, Gene_Name, tmpRes))
+  obj <- as.data.frame(cbind(gene_id, Gene_Name, tmpRes))
+  composite_markers[[i]] <- obj
+  write.table(composite_markers[[i]], file = paste("markers_composite_clusters_",sample,".csv", sep=""),
+              sep=",", quote = F, row.names=T, col.names = NA)
 }
+# Retrieve top 30 markers with smallest adjusted p-value in each cluster
 heatmapGenes <- Reduce("union",lapply(composite_markers, function(obj){
-  tmpGenes <- rownames(obj[order(obj$padj, decreasing = F),1:3][1:30,])
+  tmpGenes <- rownames(obj[order(obj$padj, decreasing = F),][1:30,])
   sapply(tmpGenes, function(gene) strsplit(gene,"_")[[1]][2])
 }))
-mat <- as.matrix(vennValues[,grep("rank",colnames(vennValues), value=T)])
-colnames(mat) <- gsub("rank_","",colnames(mat))
-rownames(mat) <- vennValues$Gene_Name
-mat[is.na(mat)] <- 0
-matTopMarkers <- mat[rownames(mat) %in% heatmapGenes,]
-plotHeight <- if(round(nrow(matTopMarkers)/4)<7){7}else{round(nrow(matTopMarkers)/4)}
+# Retrieve deregulated markers (adjusted p-value <=0.05 and absolute log2 fold-change >1) for each cluster
+deGenes <- Reduce("union",lapply(composite_markers, function(obj){
+  obj <- obj[abs(obj$log2FoldChange)>FC & !is.na(obj$padj) & obj$padj<=FDR,]
+  obj$Gene_Name
+}))
+# Obtain deregulated markers also in the top30 list
+deTop30 <- intersect(heatmapGenes, deGenes)
+# Get ranks for each of the clusters
+composite_matTopMarkers <-  matrix(0, nrow=length(deTop30), ncol=length(composite_markers), dimnames=list(deTop30,names(composite_markers)))
+for(i in 1:length(composite_markers)){
+  obj <- composite_markers[[colnames(composite_matTopMarkers)[i]]]
+  composite_matTopMarkers[,i] <- obj$rank[match(rownames(composite_matTopMarkers), obj$Gene_Name)]
+}
+# Plot ranks of target genes
+plotHeight <- if(round(nrow(composite_matTopMarkers)/4)<7){7}else{round(nrow(composite_matTopMarkers)/4)}
 pdf("Fig4E_heatmap_composite_clusters_ranks_top30MarkersPerCluster.pdf", height = plotHeight, width = 9)
-print(Heatmap(scale(matTopMarkers), name = "ranks", column_title = "Cluster", row_title = "Gene",
-              row_names_max_width = max_text_width(rownames(matTopMarkers), gp = gpar(fontsize = 12)),
-              column_names_max_height = max_text_width(colnames(matTopMarkers), gp = gpar(fontsize = 12))))
+print(Heatmap(scale(composite_matTopMarkers), name = "ranks", column_title = "Cluster", row_title = "Gene",
+              row_names_max_width = max_text_width(rownames(composite_matTopMarkers), gp = gpar(fontsize = 12)),
+              column_names_max_height = max_text_width(colnames(composite_matTopMarkers), gp = gpar(fontsize = 12))))
 graphics.off()
 
 # Supp Figure 4a: find top 30 markers for each ICELL8 sample, and generate a heatmap similar to 4E
+# Obtain markers for each sample in the ICELL8 dataset
 icell8_markers <- de.markers(gm = icell8$qc_data$gm,
-                  metadata =  icell8$qc$metadata,
-                  min_fc = 0,
-                  grouping_var = "Sample",
-                  transform_type = icell8$qc_data$params$gm_log_base)
-
-for(i in 1:length(res)){
-  colnames(icell8_markers[[i]])[colnames(icell8_markers[[i]])=="pval"] <- "pvalue"
-  colnames(icell8_markers[[i]])[colnames(icell8_markers[[i]])=="log_fc"] <- "log2FoldChange"
-  icell8_markers[[i]]$rank <- -log10(icell8_markers[[i]]$pvalue)*res[[i]]$log2FoldChange
-  tmpRes <- icell8_markers[[i]][order(icell8_markers[[i]]$rank), ]
+                                metadata =  icell8$qc$metadata,
+                                min_fc = 0,
+                                grouping_var = "Sample",
+                                transform_type = icell8$qc_data$params$gm_log_base)
+# Create tables of markers tested for each sample
+for(i in 1:length(icell8_markers)){
+  sample <- names(icell8_markers)[i]
+  obj <- icell8_markers[[i]]
+  colnames(obj)[colnames(obj)=="pval"] <- "pvalue"
+  colnames(obj)[colnames(obj)=="log_fc"] <- "log2FoldChange"
+  obj$pvalue[obj$pvalue==0] <- .Machine$double.xmin
+  obj$rank <- -log10(obj$pvalue)*obj$log2FoldChange
+  tmpRes <- obj[order(obj$rank), ]
   gene_id <- sapply(rownames(tmpRes), function(gene) strsplit(x = gene, split="_")[[1]][1])
   Gene_Name <- sapply(rownames(tmpRes), function(gene) strsplit(x = gene, split="_")[[1]][2])
-  res[[i]] <- as.data.frame(cbind(gene_id, Gene_Name, tmpRes))
+  obj <- as.data.frame(cbind(gene_id, Gene_Name, tmpRes))
+  icell8_markers[[i]] <- obj
+  write.table(icell8_markers[[i]], file = paste("markers_icell8_samples_",sample,".csv", sep=""),
+              sep=",", quote = F, row.names=T, col.names = NA)
 }
-heatmapGenes <- Reduce("union",lapply(res, function(obj){
-  tmpGenes <- rownames(obj[order(obj$padj, decreasing = F),1:3][1:30,])
+# Retrieve top 30 markers with smallest adjusted p-value in each sample
+heatmapGenes <- Reduce("union",lapply(icell8_markers, function(obj){
+  tmpGenes <- rownames(obj[order(obj$padj, decreasing = F),][1:30,])
   sapply(tmpGenes, function(gene) strsplit(gene,"_")[[1]][2])
 }))
-mat <- as.matrix(vennValues[,grep("rank",colnames(vennValues), value=T)])
-colnames(mat) <- gsub("rank_","",colnames(mat))
-rownames(mat) <- vennValues$Gene_Name
-mat[is.na(mat)] <- 0
-matTopMarkers <- mat[rownames(mat) %in% heatmapGenes,]
-plotHeight <- if(round(nrow(matTopMarkers)/4)<7){7}else{round(nrow(matTopMarkers)/4)}
-pdf("Fig4E_heatmap_composite_clusters_ranks_top30MarkersPerCluster.pdf", height = plotHeight, width = 9)
-print(Heatmap(scale(matTopMarkers), name = "ranks", column_title = "Cluster", row_title = "Gene",
-              row_names_max_width = max_text_width(rownames(matTopMarkers), gp = gpar(fontsize = 12)),
-              column_names_max_height = max_text_width(colnames(matTopMarkers), gp = gpar(fontsize = 12))))
+# Retrieve deregulated markers (adjusted p-value <=0.05 and absolute log2 fold-change >1) for each sample
+deGenes <- Reduce("union",lapply(icell8_markers, function(obj){
+  obj <- obj[abs(obj$log2FoldChange)>FC & !is.na(obj$padj) & obj$padj<=FDR,]
+  obj$Gene_Name
+}))
+# Obtain deregulated markers also in the top30 list
+deTop30 <- intersect(heatmapGenes, deGenes)
+# Get ranks for each of the samples
+icell8_matTopMarkers <-  matrix(0, nrow=length(deTop30), ncol=length(icell8_markers), dimnames=list(deTop30,names(icell8_markers)))
+for(i in 1:length(icell8_markers)){
+  obj <- icell8_markers[[colnames(icell8_matTopMarkers)[i]]]
+  icell8_matTopMarkers[,i] <- obj$rank[match(rownames(icell8_matTopMarkers), obj$Gene_Name)]
+}
+# Plot ranks of target genes
+plotHeight <- if(round(nrow(icell8_matTopMarkers)/4)<7){7}else{round(nrow(icell8_matTopMarkers)/4)}
+pdf("SuppFig4a_heatmap_icell8_samples_ranks_top30MarkersPerCluster.pdf", height = plotHeight, width = 9)
+print(Heatmap(scale(icell8_matTopMarkers), name = "ranks", column_title = "Sample", row_title = "Gene",
+              row_names_max_width = max_text_width(rownames(icell8_matTopMarkers), gp = gpar(fontsize = 12)),
+              column_names_max_height = max_text_width(colnames(icell8_matTopMarkers), gp = gpar(fontsize = 12))))
 graphics.off()
 
 # Supp Figure 4b: Venn diagram of top 30 CellenONE and ICELL8 markers
+require(VennDiagram)
+# Plot Venn diagram overlapping markers from both datasets
+pdf("SuppFig4b_Venn.pdf")
+grid.draw(venn.diagram(x = list("CellenONE-ICELL8"=rownames(composite_matTopMarkers),
+                                "ICELL8"=rownames(icell8_matTopMarkers)), col = "transparent",
+                       fontfamily ="sans",cat.fontfamily="sans",
+                       fill = c("blue", "yellow"),# fill = c("blue", "green","red"),
+                       alpha = 0.5, cat.cex=1.5, cex=1.5, height=100, width=100,filename=NULL,
+                       print.mode=c("raw","percent")))
+graphics.off()
