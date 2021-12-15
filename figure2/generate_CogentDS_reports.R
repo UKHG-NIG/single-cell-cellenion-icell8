2f08556d032e4945e2b0e3c9ee9981e23117c017
Gabriela Salinas <95362308+UKHG-NIG@users.noreply.github.com>
2021-12-15 11:12:20 +0100

    Update generate_CogentDS_reports.R

--- figure2/generate_CogentDS_reports.R
+++ figure2/generate_CogentDS_reports.R
@@ -12,10 +12,10 @@ close(fileConn)
 
 standard_analysis(gm_loc = "GSM5411464_geneMatrix_files.txt", metadata_loc = "GSM5411464_metadata_files.txt", gene_info_loc="GSE179204_gene_info.csv",
                   cluster_analysis=T, grouping_var="Sample", names=c("chip1","chip2","chip3"), report=T, CogentAP_data = T, output_dir = "CogentDS_ICELL8",
-                  qc_cell_abslowcov=5000, qc_cells_abslowgenecount=200, mito_frac=c(0,1), intergenic_frac=c(0,1), auto_outlier_removal=F)
+                  qc_cell_abslowcov=5000, mito_frac=c(0,1), intergenic_frac=c(0,1), auto_outlier_removal=F)
 
 # CellenONE/ICELL8 dataset
 ## Default analysis for samples
 standard_analysis(gm = "GSM5411467_CellenONE_geneMatrix.csv", metadata = "GSM5411467_CellenONE_metadata.csv", gene_info_loc="GSM5411467_gene_info.csv",
                   cluster_analysis=T, grouping_var="Sample", report=T, CogentAP_data = T, output_dir = "CogentDS_CellenONE",
-                  qc_cell_abslowcov=5000, qc_cells_abslowgenecount=200, mito_frac=c(0,1), intergenic_frac=c(0,1), auto_outlier_removal=F)
+                  qc_cell_abslowcov=5000, mito_frac=c(0,1), intergenic_frac=c(0,1), auto_outlier_removal=F)
